2ed205d0440e0af3493e68d5d6f6cc2898effac1
Shades <shades355@gmailcom>
2021-08-25 19:06:41 -0400

    User Input

---
+++ w3schools/04-User_Input/01-userNumbre.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int userNum;
+
+  cout << "Type a number: "  // Type a number and press enter
+  cin >> userNum;            // Get user input from the keyboard
+  cout << "Your number is: " << userNum; // Display the input value
+}
