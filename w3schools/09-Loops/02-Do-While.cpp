7ee8cdeae9f9c266c25db94bc23655a8cf74a925
Shades <shades355@gmailcom>
2021-08-25 22:12:10 -0400

    Loop practice

---
+++ w3schools/09-Loops/02-Do-While.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+// unlike a While Loop, a Do While Loop
+// will always run at least once
+
+int main() {
+
+  int i = 0
+
+  do {
+    cout << i << "\n";
+    i++
+  }
+  while (i <5)
+
+  return 0
+}
