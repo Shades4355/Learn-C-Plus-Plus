0659ea91e947aa46d1130af1b08d10078b2ef099
Shades <shades355@gmailcom>
2021-08-25 21:33:10 -0400

    String practice pt 2

---
+++ w3schools/06-Strings/05-Accessing_Strings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  string hello = "hello"
+
+  char h = hello[0]
+  cout << h // "h"
+
+  char e = hello[1]
+  cout << e // "e"
+
+  string myString = "hello"
+  myString[0] = "j"
+  cout << myString // "jello"
+}
