e11f85188c3f8d6c1f3b5f357c6b6861a8bd3e1c
Shades <shades355@gmailcom>
2021-08-25 22:03:10 -0400

    If, Else, Switch practice

---
+++ w3schools/08-Conditions/02-Else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int time = 20;
+
+  if (time < 18) {
+    cout << "Good day.";
+  } else {
+    cout << "Good morning.";
+  }
+
+  return 0
+}
---
+++ w3schools/08-Conditions/09-Shorthand-If-Else.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  int time = 20;
+  string result = (time < 18) ? "Good day." : "Goood evening."
+
+  cout << result
+
+  return 0
+}
